cd32ce9eef8fb76c667cecb2a9a1888889ba02d7
Ahmed Hussin <ahmed208087@gmail.com>
2024-05-08 12:38:44 +0300

    adding the loading effect

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function loading() {
+  return (
+    <div className='flex justify-center'>
+        <img src='spinner.png' alt = 'Loading'/>
+    </div>
+  )
+}
+
+export default loading
